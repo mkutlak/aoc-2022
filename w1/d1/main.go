bc1c4fbd499b0dc9f44f4f81687468b68a20611c
Martin Kutlak <mkutlak@users.noreply.github.com>
2022-12-16 23:52:53 +0100

    Two starts - lets Go

--- w1/d1/main.go
+++ w1/d1/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -41,6 +42,7 @@ func main() {
 	calorieTracker = append(calorieTracker, caloriesPerElf)
 	maximumCalories = findMax(calorieTracker)
 	fmt.Printf("Fattest ELF: %d\n", maximumCalories)
+	fmt.Printf("Top 3 total calories: %d\n", countTopThreeEaters(calorieTracker))
 }
 
 func findMax(l []int) int {
@@ -52,4 +54,16 @@ func findMax(l []int) int {
 		}
 	}
 	return m
+}
+
+func countTopThreeEaters(l []int) int {
+	var topThreeCalories, length int
+	length = len(l)
+
+	sort.Ints(l)
+
+	for x := 1; x <= 3; x++ {
+		topThreeCalories += l[length-x]
+	}
+	return topThreeCalories
 }
